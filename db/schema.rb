243a6f135f0f018484a3524f38ce758ddf309f6e
Raghu Betina <rbetina@gmail.com>
2013-02-04 10:55:10 -0600

    Finished switching from string 'director' column to integer 'director_id' column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130204164927) do
+ActiveRecord::Schema.define(:version => 20130204165403) do
 
   create_table "directors", :force => true do |t|
     t.string   "name"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(:version => 20130204164927) do
 
   create_table "movies", :force => true do |t|
     t.string   "title"
-    t.string   "director"
     t.integer  "year"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
