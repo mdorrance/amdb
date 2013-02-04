d7d6a2cac3a1ad276f2745dd2e40a419b4591f10
Raghu Betina <rbetina@gmail.com>
2013-02-04 09:43:53 -0600

    rails new, rails g scaffold director name:string dob:date

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amdb::Application.config.secret_token = '56b7680e14dd5df2e15a9deb5dbb32a2e5d3d0f12e70b42b61c2e8c9c39ba0a57ba918fc4a1a96a04ff761d1f9e8c9aa91c06e7dcf5470ee5104ae4ef707855e'
